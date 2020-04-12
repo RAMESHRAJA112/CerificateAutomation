191831f0f1d2034c4ab2cc9ec09ffeeb51694f23
RAMESHRAJA112 <46318841+RAMESHRAJA112@users.noreply.github.com>
2020-04-12 20:37:30 +0530

    Update renewCertificates.ps1

--- renewCertificates.ps1
+++ renewCertificates.ps1
@@ -1,6 +1,6 @@
-
+#Note Give your Certificate No Pass Phrase ..
 #Firstly export the certificate to renew it
-Export-ACMCertificate -CertificateArn arn:aws:acm:us-east-1:055309605239:certificate/69f8e78a-b43e-4e57-baa7-550267626cd3 -Passphrase Test@1234 -ProfileName test
+Export-ACMCertificate -CertificateArn arn:aws:acm:us-east-1:055309605239:certificate/xxxxxxa-b43e-4e57-baa7-xxxxxx -Passphrase xxxxx -ProfileName test
 
 #Now request AWS to renew the certificate
-Invoke-ACMCertificateRenewal -CertificateArn arn:aws:acm:us-east-1:055309605239:certificate/69f8e78a-b43e-4e57-baa7-550267626cd3 -ProfileName test
+Invoke-ACMCertificateRenewal -CertificateArn arn:aws:acm:us-east-1:055309605239:certificate/xxxxxxa-b43e-4e57-baa7-550267626cd3 -ProfileName xxxx
